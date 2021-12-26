06abe313247524027f5a83085d7132ac86719679
NeoHvZ <hvrorschach@gmail.com>
2021-12-26 12:55:53 +0800

    chore: update

---
+++ src/algorithm/leetcode/problem_1078/Solution.java
@@ -0,0 +1,19 @@
+package algorithm.leetcode.problem_1078;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public String[] findOcurrences(String text, String first, String second) {
+        String[] words = text.split(" ");
+        if (words.length < 3) {
+            return new String[]{};
+        }
+        ArrayList<String> ret = new ArrayList<>();
+        for (int i = 0; i < words.length - 2; i++) {
+            if (words[i].equals(first) && words[i + 1].equals(second)) {
+                ret.add(words[i + 2]);
+            }
+        }
+        return ret.toArray(String[]::new);
+    }
+}
